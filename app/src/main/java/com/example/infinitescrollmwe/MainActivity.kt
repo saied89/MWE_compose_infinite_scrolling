9c80b88c7649621f4e43d495fdf0fefeca84ac3b
Saeed Momenzadeh <saied.momen@balad.ir>
2021-08-05 20:20:59 +0430

    use objects in page data

--- app/src/main/java/com/example/infinitescrollmwe/MainActivity.kt
+++ app/src/main/java/com/example/infinitescrollmwe/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.infinitescrollmwe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
@@ -10,8 +11,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.paging.*
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.example.infinitescrollmwe.ui.theme.InfiniteScrollMWETheme
@@ -19,6 +23,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val flow = Pager(PagingConfig(30), 0) {
@@ -35,35 +40,63 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun InfiniteScroll(flow: Flow<PagingData<String>>) {
+fun InfiniteScroll(flow: Flow<PagingData<PageItem>>) {
     val items = flow.collectAsLazyPagingItems()
     LazyColumn(Modifier.fillMaxWidth()) {
         items(
             items,
-            key = {
-                it
+            key = { it.key }
+        ) { item ->
+            when (item?.index) {
+                null -> {
+                    this@LazyColumn.item {
+                        Text(text = "Page: ${item?.page}. key: ${item?.key}")
+                    }
+                }
+                else -> {
+                    this@LazyColumn.item {
+                        Text(text = "Page: ${item.page}. Item: ${item.index}. key: ${item.key}")
+                    }
+                }
             }
-        ) {
-            Text(text = it!!)
         }
     }
 }
 
-object DaysMockPagingSource : PagingSource<Int, String>() {
+data class PageItem(val page: Int, val index: Int?)
 
-    override fun getRefreshKey(state: PagingState<Int, String>): Int? {
+val PageItem.key: String
+    get() = when (this.index) {
+        null -> this.page.toString()
+        else -> "${this.page} + ${this.index}"
+    }
+
+object DaysMockPagingSource : PagingSource<Int, PageItem>() {
+
+    override fun getRefreshKey(state: PagingState<Int, PageItem>): Int? {
         val anchorPosition = state.anchorPosition
         return if (anchorPosition != null)
-            state.anchorPosition?.div(10)
+            state.closestItemToPosition(anchorPosition)?.page
         else
             null
     }
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, String> {
+    @ExperimentalStdlibApi
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PageItem> {
         delay(500)
+        val pageKey = params.key
+        val res = buildList<PageItem> {
+            pageKey?.let {
+                add(PageItem(pageKey, null))
+                repeat(30) {
+                    add(PageItem(pageKey, it))
+                }
+            }
+        }
         return LoadResult.Page(
-            (1..10).map { "PAGE ${params.key}: Index $it" },
+            res,
             params.key?.minus(1),
             params.key?.plus(1)
         )
